2fc11f142f03bcd33bf0e2f78ad4c884a663081c
Carlos Santos <carlos.santos@i3television.es>
2017-02-24 18:04:19 +0100

    Update tren1.js

--- tren1.js
+++ tren1.js
@@ -1,13 +1,29 @@
-var trenes_status_service=["OK","OK","OK","NOK","NOK","NOK","NOK","NOK"];
-
-//for(i=0;i<length.trenes_status_service;i++)
-//if(trenes[i]=="OK") console.log("El tren número"+i+"esta funcionando");
+var trenes=[
+["OK","D","NF"],
+["OK","D","NF"],
+["OK","D","NF"],
+["OK","N","F"],
+["NOK","D","NF"],
+["NOK","D","NF"],
+["NOK","D","NF"],
+["NOK","D","NF"],
+["OK","D","NF"],
+["OK","D","NF"],
+["OK","D","NF"],
+["OK","D","NF"],
+["OK","D","NF"],
+];
 
 
 
 function logger(element, index, array) {
-    if(element=="OK") console.log("El tren número "+index+" esta funcionando");
+    if(element[0]=="OK") console.log("El tren número "+(index+1)+" esta funcionando");
     else console.log("El tren número "+index+" no esta funcionando");
+    if(element[1]=="N") console.log("El tren número "+(index+1)+" es nocturno");
+    if(element[2]=="F") console.log("El tren número "+(index+1)+" es fiestero");
 
 }
-trenes_status_service.forEach(logger);
+
+
+
+trenes.forEach(logger);
