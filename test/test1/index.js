21c85efc45b6701eaa2d6da2abdb5fdb448443ef
Carlos Santos <carlos.santos@i3television.es>
2017-04-17 17:27:23 +0200

    Add files via upload

--- test/test1/index.js
+++ test/test1/index.js
@@ -9,9 +9,9 @@ var url = "google.es";
             res.end("La respuesta de " +url+" es "+resOrigen.statusCode );
             console.log("La respuesta de " +url+" es "+resOrigen.statusCode );
 
-        }).listen(process.env.PORT, process.env.IP);
+        }).listen(8888, 'localhost');
 
-        console.log('Servidor disponible en http://'+process.env.IP+':'+process.env.PORT+'/');
+        console.log('Servidor disponible en http://localhost:8888/');
 
 
     }).on('error', function(e) {
@@ -21,7 +21,7 @@ var url = "google.es";
             res.writeHead(200, {'Content-Type': 'text/plain'});
             res.end("La respuesta de " +url+" genera un error - " + e.message  );
 
-        }).listen(process.env.PORT, process.env.IP);
+        }).listen(8888, 'localhost');
 
         console.log('Servidor disponible en http://'+process.env.IP+':'+process.env.PORT+'/');
         console.log("Tenemos un error!! - " + e.message);
